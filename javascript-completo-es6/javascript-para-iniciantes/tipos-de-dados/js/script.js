9f97c7a28cf4d82c011b2f84611a891c3119237f
Itamar Silva <itamarsilvacc@gmail.com>
2018-12-07 01:59:16 -0200

    Code JS6 - JavaScript para Iniciantes - Tipos de Dados

---
+++ javascript-completo-es6/javascript-para-iniciantes/tipos-de-dados/js/script.js
@@ -0,0 +1,26 @@
+// Declare uma variável contendo uma string
+var nome = "Itamar Silva";
+console.log(typeof nome);
+
+// Declare uma variável contendo um número dentro de uma string
+var idadeString = "28";
+console.log(typeof idadeString);
+
+// Declare uma variável com a sua idade
+var idade = 28;
+console.log(typeof idade);
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = "Itamar",
+    sobrenome = "Silva";
+var nomeCompleto = `${nome} ${sobrenome}`;
+console.log(nomeCompleto);
+console.log(typeof nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+var frase = "It's time";
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof frase);
