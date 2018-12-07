79f260853bf23fd94309dac50d89f518b50494b0
Itamar Silva <itamarsilvacc@gmail.com>
2018-12-07 00:40:38 -0200

    Init Javascript Completo ES6. Code exercício Variáveis.

---
+++ javascript-completo-es6/javascript-para-iniciantes/variaveis/js/script.js
@@ -0,0 +1,19 @@
+// Declarar uma variável com o seu nome
+var nome = "Itamar Silva";
+console.log(nome);
+
+// Declarar uma variável com a sua idade
+var idade = 28;
+console.log(idade);
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita;
+console.log(comidaFavorita);
+
+// Atribuir valor a sua comida favorita
+comidaFavorita = "Não sei";
+console.log(comidaFavorita);
+
+// Declarar 5 variáveis diferentes sem valores
+var time, nomePai, nomeMae, sexo, nacionalidade;
